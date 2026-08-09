7306c7f3ab1244fe0b0de5c5620a6c3df6c4fac8
SachinYadavAug20 <samtagon777@gmail.com>
2026-08-09 16:45:17 +0530

    command fix

--- .nvim.lua
+++ .nvim.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<F5>", ":w<CR>:!make && ./game<CR>", { noremap = true, silent = false })
-vim.keymap.set("n", "<F6>", ":w<CR>:!make debug && ./game<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<F5>", ":w<CR>:!make && ./build/game<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<F6>", ":w<CR>:!make debug && ./build/game<CR>", { noremap = true, silent = false })
